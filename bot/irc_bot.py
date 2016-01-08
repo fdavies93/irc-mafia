9b51476d8a41f66eec1cb7daaa20513d6e879dd2
fdavies93 <fdavies93@gmail.com>
2016-01-08 20:00:50 +0000

    update from 02/01/16

--- bot/irc_bot.py
+++ bot/irc_bot.py
@@ -8,27 +8,38 @@ class vote:
 		self.peopleVoting = [] #people allowed to vote
 		self.choices = [] #list of voteOptions available to vote for
 		self.onFinish = callback #a function taking a list of choices
+		self.hasSpoofing = False
+		self.voterMessage = ""
+		self.nonVoterMessage = ""
+		#is private?
 		self.manager = myManager #a reference to the manager in which this vote is contained
 
 	def createBinaryChoice(self):
 		self.choices = [voteOption("yes"),voteOption("no")]
 
 	def runVote(self):
-		voterIndex = len(self.peopleVoting) - 1
-		choiceIndex = 0
-		while(voterIndex >= 0):
-			print self.peopleVoting[voterIndex], "make your choice!"
-			cur_input = raw_input(">> ")
-			while(choiceIndex < len(self.choices)):
-				if(cur_input == self.choices[choiceIndex].name):
-					self.choices[choiceIndex].votes += 1
-					self.peopleVoting.pop(voterIndex)
-					voterIndex -= 1
-					break
-				choiceIndex += 1
-			if(choiceIndex >= len(self.choices)):
-				print "Invalid choice, choose again!"
-				choiceIndex = 0
+		#isVoter = False
+		choiceAccepted = False
+		#choiceIndex = 0
+		for person in self.manager.people:
+			isVoter = False
+			for voter in self.peopleVoting:
+				if(voter == person):
+					isVoter = True
+			if(isVoter and person.isDead == False):
+				self.manager.interface.sendMessageToPlayer(self.voterMessage, person)
+				while(choiceAccepted == False):
+					player_input = self.manager.interface.waitForMessageFromPlayer(person)
+					choiceIndex = 0
+					while(choiceIndex < len(self.choices)):
+						if(player_input.message == self.choices[choiceIndex].name):
+							self.choices[choiceIndex].votes += 1
+							choiceAccepted = True
+						choiceIndex += 1
+					if(choiceAccepted == False):
+						self.manager.interface.sendMessageToPlayer("That choice is not valid, choose again.")
+			elif(isVoter == False and self.hasSpoofing and person.isDead == False):
+				self.manager.interface.sendMessageToPlayer(self.nonVoterMessage, person)
 		self.onFinish(self.choices)
 
 class voteOption:
@@ -39,7 +50,7 @@ class voteOption:
 class person:
 	def __init__(self, name):
 		self.name = name #string
-		self.type = "Villager" # "Villager" / "Werewolf"
+		self.type = "Villager" # "Villager" / "Werewolf" / "WitchDoctor"
 		self.isChief = False #boolean
 		self.lover = None #another player or None
 		self.isDead = False #boolean
@@ -47,17 +58,22 @@ class person:
 _PLAYERS_TO_START_GAME_ = 3
 
 class abstractInterface:
-	def __init__(self):
+	def __init__(self,myManager):
 		self.hostname = ""
 		self.port = 6667
 		self.nick = "bot"
+		self.manager = myManager
 	def connect(self):
+		return
+	def setupGame(self):
 		return
 	def sendMessageToAll(self, message):
 		return
 	def sendMessageToPlayer(self, message, player):
 		return
-	def waitForMessage(self):
+	#send message to people
+	def waitForMessageFromPlayer(self, player):
+		#wait for private message and public message from player x
 		return
 	def parseMessage(self, message):
 		return
@@ -68,18 +84,53 @@ class messageData:
 		self.sender = sender
 
 class consoleInterface(abstractInterface):
+	def setupGame(self):
+		ready = False
+		while (ready == False):
+			self.sendMessageToAll("There are currently " + str(len(self.manager.people)) + " players in the game.")
+			if(len(self.manager.people) < _PLAYERS_TO_START_GAME_):
+				self.sendMessageToAll("Too few players to begin game.")
+				self.sendMessageToAll("Type your name, or quit to quit.")
+			else:
+				self.sendMessageToAll("Type your name, or begin to start game.")
+			cur_input = self.waitForMessage().message
+			#############################
+			#make quit not an option
+			if(len(self.manager.people) >= _PLAYERS_TO_START_GAME_ and cur_input == "begin"):
+				ready = True
+			#this should be changed so that you need to ask all players' permission
+			#before you start the game
+			#CHANGE WHEN IMPLEMENTING IRC
+			else:
+				personIndex = 0
+				while personIndex < len(self.manager.people):
+					if(self.manager.people[personIndex].name == cur_input):
+						self.sendMessageToAll("Name is already taken!")
+						break
+					personIndex += 1
+				if(personIndex >= len(self.manager.people)):
+					new_person = person(cur_input)
+					self.manager.people.append(new_person)
+					self.sendMessageToAll("Person "+new_person.name+" added.")
+		########################################################
+
 	def sendMessageToAll(self, message):
 		print message
 
 	def sendMessageToPlayer(self, message, player):
 		print "Please get", player.name, "to come to the keyboard."
 		raw_input("Press Enter when ready.")
+		self.clearConsole()
 		print message
 		raw_input("Press Enter to clear.")
 		self.clearConsole()
 
 	def waitForMessage(self):
 		messageToReturn = messageData(raw_input(">> "), None)
+		return messageToReturn
+
+	def waitForMessageFromPlayer(self, player):
+		messageToReturn = messageData(raw_input(">> "), player)
 		return messageToReturn
 
 	def clearConsole(self):
@@ -99,39 +150,10 @@ class manager:
 		self.dayNumber = 0
 		self.isQuit = False #has the game quit?
 		self.people = [] #LIST of people
-		self.interface = consoleInterface()
+		self.interface = consoleInterface(self)
 	def setupGame(self):
-
 		ready = False
-		#this is the part that could be moved to interface
-		while (ready == False):
-			self.interface.sendMessageToAll("There are currently " + str(len(self.people)) + " players in the game.")
-			if(len(self.people) < _PLAYERS_TO_START_GAME_):
-				self.interface.sendMessageToAll("Too few players to begin game.")
-				self.interface.sendMessageToAll("Type your name, or quit to quit.")
-			else:
-				self.interface.sendMessageToAll("Type your name, or begin to start game.")
-			cur_input = self.interface.waitForMessage().message
-			#############################
-			#make quit not an option
-			if(len(self.people) >= _PLAYERS_TO_START_GAME_ and cur_input == "begin"):
-				ready = True
-			#this should be changed so that you need to ask all players' permission
-			#before you start the game
-			#CHANGE WHEN IMPLEMENTING IRC
-			else:
-				personIndex = 0
-				while personIndex < len(self.people):
-					if(self.people[personIndex].name == cur_input):
-						print "Name is already taken!"
-						break
-					personIndex += 1
-				if(personIndex >= len(self.people)):
-					new_person = person(cur_input)
-					self.people.append(new_person)
-					print "Person", new_person.name, "added."
-		########################################################
-
+		self.interface.setupGame()
 		#Set up players with correct classes (werewolves / villagers)
 		num_werewolves = round(len(self.people) / 3)
 		while (num_werewolves > 0):
@@ -142,7 +164,7 @@ class manager:
 				#print self.people[choice].name, "is now a werewolf!"
 
 		#add code so it tells everyone what they are
-		for player in self.players:
+		for player in self.people:
 			self.interface.sendMessageToPlayer("You are a " +player.type+"!",player)
 		self.interface.sendMessageToAll("Prepare to play Werewolf!")
 
@@ -163,10 +185,11 @@ class manager:
 		if(self.dayNumber != 0):
 			#set up and run kill vote
 			killVote = vote(self,self.resolveKillVote)
+			killVote.voterMessage = "Choose who you think is the werewolf."
 			for person in self.people:
 				if person.isDead == False:
 					killVote.choices.append(voteOption(person.name))
-					killVote.peopleVoting.append(person.name)
+					killVote.peopleVoting.append(person)
 			self.interface.sendMessageToAll("Time to vote on whom we would like to kill!")
 			killVote.runVote()
 			self.testForVictory()
@@ -179,14 +202,21 @@ class manager:
 
 
 	def runNight(self):
-		print "Night has fallen!"
+		self.interface.sendMessageToAll("Night has fallen!")
 		werewolfVote = vote(self, self.resolveKillVote)
+		werewolfVote.voterMessage = "Who would you like to kill?"
+		werewolfVote.nonVoterMessage = "Stay asleep, sheeple."
+		werewolfVote.hasSpoofing = True
 		for person in self.people:
 			if(person.type == "Werewolf" and person.isDead == False):
-				werewolfVote.peopleVoting.append(person.name)
+				werewolfVote.peopleVoting.append(person)
 			elif(person.isDead == False):
 				werewolfVote.choices.append(voteOption(person.name))
-		print "Werewolves, choose your victims!"
+		#for person in self.people:
+		#	if(person.type == "Werewolf"):
+		#		self.interface.sendMessageToPlayer("Werewolf, choose your victim!", person)
+		#	else:#generic villager message
+		#		self.interface.sendMessageToPlayer("Villager, stay asleep and await the slaughter!", person)
 		werewolfVote.runVote()
 		self.testForVictory()
 
@@ -199,10 +229,10 @@ class manager:
 			elif(person.type == "Werewolf" and person.isDead == False):
 				numWerewolves += 1
 		if(numWerewolves == 0):
-			print "Game over, villagers win!"
+			self.interface.sendMessageToAll("Game over, villagers win!")
 			self.isQuit = True
 		elif(numVillagers == 0):
-			print "Game over, werewolves win!"
+			self.interface.sendMessageToAll("Game over, werewolves win!")
 			self.isQuit = True
 
 	# This is dramatic!
@@ -220,7 +250,7 @@ class manager:
 		for person in self.people:
 			if(person.name == personToKill):
 				person.isDead = True #DRAMA
-				print person.name , "is dead!"
+				self.interface.sendMessageToAll(person.name + "is dead!")
 
 	def voteCount(self,choices):
 		for choice in choices:
